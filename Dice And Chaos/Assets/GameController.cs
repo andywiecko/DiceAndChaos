0dc96b51609eff0958149e4422286e90f199879d
andywiecko <andywiecko@gmail.com>
2020-02-28 17:44:07 +0100

    button added, logger draft added

--- Dice And Chaos/Assets/GameController.cs
+++ Dice And Chaos/Assets/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameController : MonoBehaviour
 {
@@ -18,27 +19,73 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private Vector3 initialAngularVelocity;
 
+    private Vector3 position;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+
+    [SerializeField]
+    public bool trackCamera = false;
+    public Camera mainCamera;
+    private Vector3 initialCameraDistance;
+
+
+
     bool messageRecived = false;
 
-    
+
 
     void Start()
     {
         Vector3 position = gameObject.transform.position;
         specimen = Instantiate(specimen, position, Quaternion.identity);
+        initialCameraDistance = position - mainCamera.transform.position;
+        initialPosition = specimen.transform.position;
+        initialRotation = specimen.transform.rotation;
         rb = specimen.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 100f;
+
+
+        /*
+        Time.maximumParticleDeltaTime = 0.03f;
+        Time.timeScale = 30f;
+        Time.maximumDeltaTime = 0.01f;
+        /*/
+
+
+
+
+
+        RollADice();
+
+
+
+
+    }
+
+    public void RollADice()
+    {
+        //specimen.transform.position = position;
+
+        specimen.transform.position = initialPosition;
+        specimen.transform.rotation = initialRotation;
+
         rb.velocity = new Vector3(initialVelocity, 0, 0);
         rb.angularVelocity = initialAngularVelocity;
-        //Time.maximumParticleDeltaTime = 0.03f;
-        //Time.timeScale = 30f;
-        //Time.maximumDeltaTime = 0.01f;
-        
-        
+
+        messageRecived = false;
+
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (trackCamera)
+            mainCamera.transform.position = specimen.transform.position - initialCameraDistance;
         velocity = rb.velocity;
         if (velocity == Vector3.zero && !messageRecived)
         {
@@ -50,17 +97,17 @@ public class GameController : MonoBehaviour
             foreach (Transform child in specimen.transform)
             {
                 float childheigth = child.transform.position.y;
-                
+
                 if (maxheight < childheigth)
                 {
                     maxheight = childheigth;
                     maxname = child.name;
                 }
-                
-                
+
+
             }
             Debug.Log($"You roll { maxname }!");
-
+            //Logger.Save("test");
 
         }
     }
---
+++ Dice And Chaos/Assets/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Logger 
+{
+    public static void Save(string content)
+    {
+        string path = Application.persistentDataPath + "/test.txt";
+        Debug.Log(path);
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(content);
+        writer.Close();
+    }
+}
